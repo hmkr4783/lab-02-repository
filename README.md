2fd240f6489817a3e4c6b914de9c8674174debd8
ryankrdh <rhk4783@gmail.com>
2019-04-09 20:34:12 -0700

    feature 1 completed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # lab-02-repository
+
+## Feature 1
+Number and name of feature: Feature #1: Display Images
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 7:30pm
+
+Finish time: 8:30pm
+
+Actual time needed to complete: 1hr.
+
+## Feature 2
+Number and name of feature: Feature #2: Filter Images
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 8:40pm
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function Horn(horn) {
+    this.title = horn.title;
+    this.image_url = horn.image_url;
+    this.description = horn.description;
+    this.keyword = horn.keyword;
+    this.horns = horn.horns;
+}
+
+Horn.allPhotos = [];
+
+Horn.prototype.render = function() {
+    $('main').append('<div class="clone"></div>');
+    let hornClone = $('div[class="clone"]');
+
+    let hornHtml = $('#photo-template').html();
+
+    hornClone.html(hornHtml)
+
+    hornClone.find('h2').text(this.title);
+    hornClone.find('img').attr('src', this.image_url);
+    hornClone.find('p').text(this.description);
+    hornClone.removeClass('clone');
+    hornClone.attr('class', this.title);
+}
+
+Horn.readJson = () => {
+    $.get('../data/page-1.json', 'json')
+        .then(data => {
+            data.forEach(item => {
+                Horn.allPhotos.push(new Horn(item));
+            })
+        })
+        .then(Horn.loadPhotos)
+}
+
+Horn.loadPhotos = () => {
+    Horn.allPhotos.forEach(horn => horn.render())
+}
+
+$(() => Horn.readJson());
